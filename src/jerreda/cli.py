60bd36d22f105f0e9d0c15c3143c0374b1ee2d2c
vhzkclq0705 <vhzkclq0705@naver.com>
2025-01-20 17:36:59 +0900

    직접 입력받도록 수정

--- src/jerreda/cli.py
+++ src/jerreda/cli.py
@@ -1,8 +1,8 @@
+import typer
 from president_speech.db.parquet_interpreter import get_parquet_full_path
 import pandas as pd
 
-def group_by_count():
-    kw = "올림픽"
+def group_by_count(kw: str):
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
     f_df = df[df['speech_text'].str.contains(kw, case=False)]
@@ -10,3 +10,9 @@ def group_by_count():
     sdf = rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
     
     print(sdf.to_string(index=False))
+
+def main(kw: str):
+    group_by_count(kw)
+
+if __name__ == "__main__":
+    typer.run(main)
