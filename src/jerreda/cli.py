068a4599a432083685e7d4f92309eedeb9f544e9
vhzkclq0705 <vhzkclq0705@naver.com>
2025-01-20 17:08:49 +0900

    cli파일 수정

--- src/jerreda/cli.py
+++ src/jerreda/cli.py
@@ -1,2 +1,12 @@
+from president_speech.db.parquet_interpreter import get_parquet_full_path
+import pandas as pd
+
 def group_by_count():
-    print("TODO")
+    kw = "올림픽"
+    data_path = get_parquet_full_path()
+    df = pd.read_parquet(data_path)
+    f_df = df[df['speech_text'].str.contains(kw, case=False)]
+    rdf = f_df.groupby('president').size().reset_index(name='count')
+    sdf = rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
+    
+    print(sdf)
